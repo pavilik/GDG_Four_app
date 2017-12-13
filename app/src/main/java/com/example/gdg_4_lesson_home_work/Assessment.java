b77e56674283d358b08135fb831676a8922eacf9
Pavilik <paveltempmail@gmail.com>
2017-12-13 10:01:06 +0300

    Исправлены ошибки: генерации оценок, вывода данных в консоль. пункт 1 домашнего задания выполнен в полном объеме.

--- app/src/main/java/com/example/gdg_4_lesson_home_work/Assessment.java
+++ app/src/main/java/com/example/gdg_4_lesson_home_work/Assessment.java
@@ -1,31 +1,36 @@
 package com.example.gdg_4_lesson_home_work;
 
+import static java.lang.Math.*;
+
 /**
  * Created by Павел on 12.12.2017.
  */
 //абстрактный класс оценивания
-public abstract class Assessment {
-    int  field1; //оценка в числовом формате(A=5, B=4, C=3, D=2, F=1)
-    String field2="no_name"; //имя ученика
-    boolean field3= true; ///флаг выставления оценки
-    enum assessments {A,  B, C, D, F}; //перечисление оценок
+public abstract class Assessment {//класс выставления оценок
+    int  field1; //оценка в числовом формате(A=5, B=4, C=3, D=2, F=1) //не использовано, зарезервированное поле для адаптации............. Ха, да просто для FUN
+    String field2="Класс номер"; //
+    boolean field3= false; ///флаг генерации журнала
+    enum assessments {A,  B, C, D, F}; //перечисление оценок //не использовано, зарезервированное поле для адаптации.
 
     public abstract void set_assessment (); //метод выставления оценки в журнал
 
     protected abstract int  check_prev_assessment(String assessment);//проверка предыдущей оценки, принемает имя оцениваемого
 
    public int teacher_mood (){//метод выставления оценок
-       int mood = (int) (Math.random()%5);
+       int mood = (int) (random()*5);
        return mood;
    }
 
-    private int fun_assessment (String student_name){//метод для дополнительных манипуляций с оценками :)
+    protected int fun_assessment (String student_name){//метод для дополнительных манипуляций с оценками :) н
       if(check_prev_assessment(student_name)>3)
           return 5;
       else return 1;
 
 
 
+    }
+    private void set_class_name(){ //метод не используется невиден никому т.к. private(сделано для выполнения задания), а вообще должен сгенерировать номер класса
+        field2=field2+"№"+ random()*10;
     }
 
 
--- app/src/main/java/com/example/gdg_4_lesson_home_work/Example_test.java
+++ app/src/main/java/com/example/gdg_4_lesson_home_work/Example_test.java
@@ -4,20 +4,30 @@ package com.example.gdg_4_lesson_home_work;
  * Created by Павел on 12.12.2017.
  */
 
-public class Example_test {
+public class Example_test { //класс для процесса теста и выставления оценок
     public static void main(String[] args) {
-        Realiz_assessments god_teacher = new Realiz_assessments();
-        System.out.println("\n Журнал до новых оценок Хороший учитель \n");
-        Bad_teacher bad_teacher =new Bad_teacher();
-        System.out.println("\n Журнал до новых оценок Плохой учитель\n");
+        Realiz_assessments god_teacher = new Realiz_assessments();//создание объекта оценок учителя норм
+        System.out.println("\nЖурнал до новых оценок Хороший учитель");
+       System.out.println("Флаг генерации журнала = "+ god_teacher.field3);
+
+        god_teacher.printjornal();
+     //   god_teacher.set_class_name(); //можно  будет использовать только после изменения видимости в классе Assessment
+
+        Bad_teacher bad_teacher =new Bad_teacher();//создание объекта оценок учителя плохого
+        System.out.println("\nЖурнал до новых оценок Плохой учитель");
+        System.out.println("Флаг генерации журнала = "+bad_teacher.field3);
+        bad_teacher.printjornal();
+     //   bad_teacher.set_class_name();//можно  будет использовать только после изменения видимости в классе Assessment
+
+
         //процессы выставления оценок
         god_teacher.set_assessment();
         bad_teacher.set_assessment();
 
         System.out.println("\n Хороший учитель:\n");
         god_teacher.printjornal();
-        System.out.println("\n Плохой учитель:\n");
 
+        System.out.println("\n Плохой учитель:\n");
         bad_teacher.printjornal();
     }
 }
