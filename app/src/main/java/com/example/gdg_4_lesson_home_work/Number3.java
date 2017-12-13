339c52c6206077ebf821e8c1d4fb7a8a815b6a24
Pavilik <paveltempmail@gmail.com>
2017-12-13 11:32:42 +0300

    Выполнение задания №2 (интерфейсы и реализация в классах). Подход формальный :)

---
+++ app/src/main/java/com/example/gdg_4_lesson_home_work/Number3.java
@@ -0,0 +1,11 @@
+package com.example.gdg_4_lesson_home_work;
+
+/**
+ * Created by Павел on 13.12.2017.
+ * формальный подход к реализации задания номер 2
+ */
+
+public interface Number3 extends Human {
+    String name = "Дженнифер";
+    boolean pol =true;
+}
