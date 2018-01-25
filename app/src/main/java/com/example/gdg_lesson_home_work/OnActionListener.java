aa863843a0324361cb1a5cab3732ee7b5712b81b
Pavilik <paveltempmail@gmail.com>
2018-01-25 09:02:49 +0300

    Очищен проект для выполнения ДЗ№8. Далее ветки стоит создавать с этого коммита.

--- app/src/main/java/com/example/gdg_lesson_home_work/OnActionListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gdg_lesson_home_work;
-
-/**
- * Created by Павел on 24.01.2018.
- */
-
-public interface OnActionListener {
-    public void onAction(String strNextFrgmtName);
-}
