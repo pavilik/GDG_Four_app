39cbbacefe6d63665efae576cff43b853dfcf59f
Pavilik <paveltempmail@gmail.com>
2018-02-05 12:09:41 +0300

    добавлено кард вью

---
+++ app/src/main/java/com/example/home_work/MainActivity2Fragment.java
@@ -0,0 +1,14 @@
+package com.example.home_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2Fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity2_fragment);
+
+    }
+}
